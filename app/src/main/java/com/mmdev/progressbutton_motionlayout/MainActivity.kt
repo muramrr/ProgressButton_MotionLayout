790e6090c13453b43e989ea7c3aeeb6c0fe82289
Andrii Kovalchuk <andrii.kovalchuk@lun.ua>
2021-04-04 15:46:52 +0300

    update and bug fixes

--- app/src/main/java/com/mmdev/progressbutton_motionlayout/MainActivity.kt
+++ app/src/main/java/com/mmdev/progressbutton_motionlayout/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.mmdev.progressbutton_motionlayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.postDelayed
 
-class MainActivity: AppCompatActivity() {
+class MainActivity: AppCompatActivity(R.layout.activity_main) {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		
+		val btnDone = findViewById<Button>(R.id.btnDone)
+		val motionContainer = findViewById<MotionLayout>(R.id.motionContainer)
+		
 		btnDone.setOnClickListener {
+			
 			motionContainer.transitionToState(R.id.middle)
-			Handler().postDelayed({motionContainer.transitionToState(R.id.scaled)}, 3000)
-			Handler().postDelayed({motionContainer.transitionToState(R.id.end)}, 3400)
+			
+			it.postDelayed(3000) {
+				motionContainer.transitionToState(R.id.scaled)
+			}
+			it.postDelayed(3400) {
+				motionContainer.transitionToState(R.id.end)
+			}
 		}
-
 
 	}
 }
